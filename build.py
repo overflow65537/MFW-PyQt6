a9b805f9b004b379d643c2257376243b91d5cf7f
overflow <err.overflow@gmail.com>
2024-12-21 21:34:25 +0800

    增加 Nuitka 版本检查并修正非 Windows 平台下 Nuitka 路径获取逻辑

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ import sys
 
 
 site_packages_paths = site.getsitepackages()
+subprocess.run(["nuitka", "--version"], check=True)
 if sys.platform == "win32":
     # 获取 Scripts 路径
     site_user_base = site_packages_paths[0]
@@ -16,8 +17,14 @@ if sys.platform == "win32":
     nuitka_path = os.path.join(scripts_path, "nuitka.cmd")
 else:
 
+    subprocess.run(["which", "nuitka"], check=True)
+    # 获取 bin 路径
+    site_user_base = site_packages_paths[0]
+    bin_path = os.path.join(os.path.dirname(site_user_base), "bin")
+    print(f"Bin path: {bin_path}")
+
     # 查找包含 nuitka 的路径
-    nuitka_path = "nuitka"
+    nuitka_path = os.path.join(bin_path, "nuitka")
 
 # 检查 nuitka 是否存在
 if not os.path.exists(nuitka_path):
