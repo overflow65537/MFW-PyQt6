c5f45873404515304d6c7821844b759be0b03296
ERR_Overflow <err.overflow@Gmail.com>
2025-01-13 10:08:29 +0800

    fix:修复构建脚本错误

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -12,7 +12,7 @@ url = "https://github.com/overflow65537/MFW-PyQt6"  # 你的項目地址
 config_name = "default"
 resource_path = os.path.join(".", "bundles", resource_name)
 config_path = os.path.join(
-    ".", "config", resource_name, config_name, "maa_pi_config.json"
+    ".", "config", resource_name, "config", config_name, "maa_pi_config.json"
 )
 print(f"title: {title}")
 print(f"resource_name: {resource_name}")
@@ -83,7 +83,7 @@ pi_config = {
     "exe_parameter": "",
 }
 install_config_path = os.path.join(
-    ".", "MFW", "config", resource_name, config_name, "maa_pi_config.json"
+    ".", "MFW", "config", resource_name, "config", config_name, "maa_pi_config.json"
 )
 os.makedirs(os.path.dirname(install_config_path), exist_ok=True)
 
