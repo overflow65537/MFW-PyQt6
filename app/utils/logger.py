fa99dfc209e739234eaa02fa97e8c717ffc72e5f
overflow <err.overflow@gmail.com>
2025-02-25 08:56:09 +0800

    perf:屏蔽urllib3防止带出敏感信息

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -4,7 +4,10 @@ import os
 # 创建日志目录
 if not os.path.exists("./debug"):
     os.makedirs("./debug")
-
+# 获取requests模块的日志记录器
+requests_logger = logging.getLogger('urllib3')
+# 关闭requests模块的日志输出
+requests_logger.setLevel(logging.CRITICAL)
 # 设置日志记录器
 logging.basicConfig(
     format="[%(asctime)s][%(levelname)s][%(filename)s][L%(lineno)d][%(funcName)s] | %(message)s",
