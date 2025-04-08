0a5215ca04019bbff452f4b7fc21dd3cf9a98de8
overflow <err.overflow@gmail.com>
2025-04-08 19:21:06 +0800

    DEV:恢复打包

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # 将当前工作目录设置为程序所在的目录，确保无论从哪里执行，其工作目录都正确设置为程序本身的位置，避免路径错误。
-# os.chdir(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False)else os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False)else os.path.dirname(os.path.abspath(__file__)))
 import argparse
 import json
 
